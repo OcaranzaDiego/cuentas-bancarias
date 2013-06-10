0a412771529df2458c23068b5a475aaa3294ef76
Diego Ocaranza <ocaranzadiego2105@gmail.com>
2013-06-10 13:17:56 -0300

    Update CajaAhorros.java

--- src/main/java/edu/tallerweb/cuentas/CajaAhorros.java
+++ src/main/java/edu/tallerweb/cuentas/CajaAhorros.java
@@ -1,22 +1,21 @@
 package edu.tallerweb.cuentas;
 
 public class CajaAhorros extends AbstractCuenta{
-
+	int cantidad = 0;
 public void depositar(final Double monto) {
 	super.depositar(monto);
 		}
 
 public void extraer(final Double monto){ 
-	int cantidadExtracciones = 0;
 	super.extraer(monto);
-	cantidadExtracciones++;
-	if ( cantidadExtracciones > 5){
+	cantidad++;
+	if ( cantidad > 5){
 	super.extraer(6.0); 
 	}
 	}
 
 public Double getSaldo() {
-	throw new RuntimeException("No implementado aÃºn");
+	return super.getSaldo();
 	}
 
 }
