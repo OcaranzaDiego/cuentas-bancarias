e5d1c59655d1ac99d8072f29d8ce649bc2ecb774
Diego Ocaranza <ocaranzadiego2105@gmail.com>
2013-06-10 00:23:56 -0300

    Update CuentaSueldo.java

--- src/main/java/edu/tallerweb/cuentas/CuentaSueldo.java
+++ src/main/java/edu/tallerweb/cuentas/CuentaSueldo.java
@@ -1,35 +1,16 @@
 package edu.tallerweb.cuentas;
 
-/**
- * Es el tipo de cuenta más simple, ya que se rige por la premisa
- * de que en tanto y en cuanto se tenga tanto o más dinero en
- * cuenta del que se quiere extraer, la operación se debe efectuar
- * correctamente.
- */
-public class CuentaSueldo {
+public class CuentaSueldo extends AbstractCuenta {
 
-	/**
-	 * No hay reglas adicionales para el depósito
-	 * @param monto a depositar
-	 */
-	public void depositar(final Double monto) {
-		throw new RuntimeException("No implementado aún");
+public void depositar(final Double monto) {
+	super.depositar(monto);
 	}
 
-	/**
-	 * No hay reglas adicionales para la extracción
-	 * @param monto a extraer
-	 */
-	public void extraer(final Double monto) {
-		throw new RuntimeException("No implementado aún");
+public void extraer(final Double monto) {
+	super.extraer(monto);
 	}
 
-	/**
-	 * Permite saber el saldo de la cuenta
-	 * @return el saldo de la cuenta
-	 */
-	public Double getSaldo() {
-		throw new RuntimeException("No implementado aún");
+public Double getSaldo() {
+	throw new RuntimeException("No implementado aÃºn");
 	}
-
 }
