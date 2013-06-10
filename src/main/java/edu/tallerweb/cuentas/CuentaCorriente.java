e1026d1fc3068d0d8e1d6c2a2e1160e3586d1982
Diego Ocaranza <ocaranzadiego2105@gmail.com>
2013-06-10 13:22:11 -0300

    Update CuentaCorriente.java

--- src/main/java/edu/tallerweb/cuentas/CuentaCorriente.java
+++ src/main/java/edu/tallerweb/cuentas/CuentaCorriente.java
@@ -2,21 +2,20 @@ package edu.tallerweb.cuentas;
 
 public class CuentaCorriente extends AbstractCuenta {
 
-private double descubiertoTotal = 150;
+
 public CuentaCorriente(final Double descubiertoTotal) {
-	throw new RuntimeException("No implementado aÃºn");
+private double descubiertoTotal = descubiertoTotal;
 }
 	
 public void depositar(final Double monto) {
-	throw new RuntimeException("No implementado aÃºn");
+	super.depositar(monto);
 }
 
 public void extraer(final Double monto) {
 	if( monto > this.getSaldo() ) {
-	Double montoConImpuesto = ( monto - this.getSaldo() ) * 1.05 ;
-	if ( montoConImpuesto <= this.descubierto ){
-	this.descubierto -= montoConImpuesto;
-	super.saldo = 0.0;
+	if ( monto <= this.descubierto ){
+	this.descubierto -= monto
+	super.setSaldo(0.0);
 	}
 	else throw new CuentaBancariaException("No cubre descubierto");
 	}
@@ -24,11 +23,11 @@ public void extraer(final Double monto) {
 	}
 
 public Double getSaldo() {
-	throw new RuntimeException("No implementado aÃºn");
+	return super.getSaldo();
 }
 	
 public Double getDescubierto() {
-	throw new RuntimeException("No implementado aÃºn");
+	return this.descubiertoTotal;
 	}
 
 }
