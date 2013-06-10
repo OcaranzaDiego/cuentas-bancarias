206f378f95d7d274e5108a985711aa6dc1a9ef1e
Diego Ocaranza <ocaranzadiego2105@gmail.com>
2013-06-10 00:24:40 -0300

    Update CajaAhorros.java

--- src/main/java/edu/tallerweb/cuentas/CajaAhorros.java
+++ src/main/java/edu/tallerweb/cuentas/CajaAhorros.java
@@ -1,35 +1,22 @@
 package edu.tallerweb.cuentas;
 
-/**
- * Similar a la CuentaSueldo, pero se pide que luego de la
- * quinta extracción de dinero se cobre un costo adicional
- * por extracción de $ 6
- */
-public class CajaAhorros {
+public class CajaAhorros extends AbstractCuenta{
 
-	/**
-	 * No hay reglas adicionales para el depósito
-	 * @param monto a depositar
-	 */
-	public void depositar(final Double monto) {
-		throw new RuntimeException("No implementado aún");
+public void depositar(final Double monto) {
+	super.depositar(monto);
+		}
+
+public void extraer(final Double monto){ 
+	int cantidadExtracciones = 0;
+	super.extraer(monto);
+	cantidadExtracciones++;
+	if ( cantidadExtracciones > 5){
+	super.extraer(6.0); 
+	}
 	}
 
-	/**
-	 * Se cobran $6 adicionales por cada extracción luego de
-	 * la quinta.
-	 * @param monto a extraer
-	 */
-	public void extraer(final Double monto) {
-		throw new RuntimeException("No implementado aún");
-	}
-
-	/**
-	 * Permite saber el saldo de la cuenta
-	 * @return el saldo de la cuenta
-	 */
-	public Double getSaldo() {
-		throw new RuntimeException("No implementado aún");
+public Double getSaldo() {
+	throw new RuntimeException("No implementado aÃºn");
 	}
 
 }
